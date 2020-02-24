3ea6093a320e728f53fe08bdb69020b0122aad12
blody09 <blakeody9@gmail.com>
2020-02-24 08:38:22 -0500

    first upload MVC

---
+++ MVCLab/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLab.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CalculationForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ResultForm(double num1, double num2, string math) // add view here
+        {
+            double result = 0; // @model double is talking to this int result!
+            if (math == "Add")
+            {
+                result = num1 + num2;
+            }
+            else if (math == "Sub")
+            {
+                result = num1 - num2;
+            }
+            else if (math == "Multiply")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+
+            return View(result); // return whatever the IAR was named + our int var Result "Result",
+        }
+    }
+}
---
+++ MVCLab/Models/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLab.Models
+{
+    public class Result
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+        public double Result1
+        {
+            get { return Num1 / Num2; }
+        }
+        public double Result2
+        {
+            get { return Num1 % Num2; }
+        }
+
+    }
+}
