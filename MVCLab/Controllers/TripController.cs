164e568482c2997b6e4457772771d052bfa0e25c
blody09 <blakeody9@gmail.com>
2020-02-24 19:19:13 -0500

    tripometer add

---
+++ MVCLab/Controllers/TripController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCLab.Models;
+
+namespace MVCLab.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult MileageForm()
+        {
+            return View();
+        }
+
+       
+        public IActionResult MileageResult(Tripometer trips)
+        {
+           
+            return View(trips);
+        }
+
+
+    }
+}
---
+++ MVCLab/Models/Tripometer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLab.Models
+{
+    public class Tripometer
+    {
+     
+            public double MPG { get; set; }
+            public double Gallons { get; set; }
+            public double Trip { get; set; }
+
+        
+    }
+}
